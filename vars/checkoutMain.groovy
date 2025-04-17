0e9c492b583169ebd092db275f2247e844bbc9c0
samueltanichip <samueltani@chiptronic.com.br>
2025-04-17 11:40:29 -0300

    Update checkoutMain.groovy

--- vars/checkoutMain.groovy
+++ vars/checkoutMain.groovy
@@ -1,6 +1,8 @@
 def call() {
-    stage('Checkout main') {
-        git branch: 'main', 
+    def branch = env.BRANCH_NAME ?: 'main' // fallback para 'main' se estiver vazio
+
+    stage("Checkout ${branch}") {
+        git branch: branch,
             url: 'https://github.com/samueltanichip/express_server_for_flutter_app_testing.git'
     }
 }
